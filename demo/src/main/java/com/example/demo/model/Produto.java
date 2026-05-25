4d910b8b03726a747f8adabc04f9b17485a7c947
WIlliam <155578165+willuchihaa@users.noreply.github.com>
2026-05-25 15:18:44 -0300

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/model/Produto.java
@@ -0,0 +1,19 @@
+package com.example.tarefa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Produto {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private int preco;
+    private String categoria;
+
+}
